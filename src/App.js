9730b9f101378ec5d560d34c45c39c3bb20d73d6
Chris Nyaga <nyagachris75@gmail.com>
2017-11-21 05:29:18 +0300

    Done all I could.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const { createApolloFetch } = require('apollo-fetch');
 
 class App extends Component {
+  doSearch(){
+    var q = document.getElementById('Q').value;
+    const fetch = createApolloFetch({
+      uri: 'https://staackle.herokuapp.com/api/v1',
+    });
+
+    fetch({
+      query: `
+        search(query: `+q+`) {
+          title,link,tags,score
+        }`,
+
+    }).then(res => {
+      console.log(res.data);
+    });
+      };
+//   {
+//   search(query: "ubuntu") {
+//     title,link,tags,score
+//   }
+// }
   render() {
     return (
       <div className="App">
@@ -15,12 +36,16 @@ class App extends Component {
       <span className="spang2">k</span>
       <span className="spanl">l</span>
       <span className="spane">e</span>
+      <span className="sorry">
+      (Wasnt able to get this to work yet. Will work on it later.
+      Backend Api is working well though.)
+      </span>
       </div>
       <div className="search">
-      <input type="Text" vname="" className="search-box" maxlength="25"/>
+      <input type="Text" id='Q' className="search-box" maxLength="25"/>
       </div>
 
-	    <div className="btn-search">
+	    <div className="btn-search" onClick={this.doSearch}>
 	    	Stoockle Search
 	    </div>
 
